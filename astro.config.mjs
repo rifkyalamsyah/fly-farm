974709a5ff23aca8986dfdf07375eb7fb19edae1
Rifky Alamsyah <rifkyalamsyah@Rifkys-MacBook-Pro.local>
2023-12-12 14:50:30 +0700

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rifkyalamsyah.github.io',
+  base: '/fly-farm',
   output: 'static',
   compressHTML: false,
   prefetch: true,
-  site: 'https://rifkyalamsyah.github.io',
-  base: '/fly-farm',
+  integrations: [
+    compress({
+      CSS: false,
+      HTML: false,
+      Image: false,
+      JavaScript: false,
+      SVG: false,
+    }),
+  ],
 });
